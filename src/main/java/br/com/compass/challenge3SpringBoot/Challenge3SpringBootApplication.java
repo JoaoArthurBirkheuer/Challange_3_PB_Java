10355c48879e419cadd2c9d11e0643d35b41b6d3
JOÃO ARTHUR BIRKHEUER <joaobirkheuer@gmail.com>
2025-05-19 13:24:23 -0300

    Configurando projeto

---
+++ src/main/java/br/com/compass/challenge3SpringBoot/Challenge3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.challenge3SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge3SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge3SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compass/challenge3SpringBoot/Challenge3SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.challenge3SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge3SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
